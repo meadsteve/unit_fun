0aeb85163497e220b4b337bd2a5a6d4260dcce29
steve.brazier <meadsteve@gmail.com>
2015-11-28 12:05:03 +0000

    make composite type order irrelevant

--- lib/unit_fun/units/composite_unit.ex
+++ lib/unit_fun/units/composite_unit.ex
@@ -7,6 +7,7 @@ defmodule UnitFun.Units.CompositeUnit do
 
   def multiply_unit(%__MODULE__{numerators: numerators}, %{type: :primitive_unit} = unit) do
     %__MODULE__{numerators: [unit | numerators]}
+      |> simplify
   end
 
   def multiply_unit(%__MODULE__{} = base, unit_module)
@@ -26,6 +27,19 @@ defmodule UnitFun.Units.CompositeUnit do
   def multiply_unit(first_module, second_module)
   when is_atom(first_module) and is_atom(second_module)
   do
-    new_with_numerator(first_module.unit) |> multiply_unit(second_module.unit)
+    new_with_numerator(first_module.unit)
+      |> multiply_unit(second_module.unit)
+  end
+
+  defp simplify(%__MODULE__{numerators: numerators}) do
+     %__MODULE__{numerators: sort_types(numerators)}
+  end
+
+  defp sort_types(numerators) do
+     numerators |> Enum.sort(&type_order/2)
+  end
+
+  defp type_order(x, y) do
+     to_string(x.__struct__) > to_string(y.__struct__)
   end
 end
--- test/unit_fun/units/composite_unit_test.exs
+++ test/unit_fun/units/composite_unit_test.exs
@@ -12,6 +12,17 @@ defmodule UnitFun.Units.CompositeUnitTests do
     expected = %CompositeUnit{numerators: [Newtons.unit, Meters.unit]}
     assert newton_meters == expected
   end
+
+
+  test "Order is not important when units are multipied together" do
+    newton_meters = Meters.unit
+      |> CompositeUnit.multiply_unit(Newtons.unit)
+
+    meter_newtons = Newtons.unit
+      |> CompositeUnit.multiply_unit(Meters.unit)
+
+    assert meter_newtons == newton_meters
+  end
 end
 
 defmodule UnitFun.Units.CompositeUnitTests.Meters do
