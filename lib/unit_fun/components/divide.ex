bca17f2420825872f8dfb2f037fb7dde80efe9af
steve.brazier <meadsteve@gmail.com>
2015-11-29 08:46:06 +0000

    convertable division

--- lib/unit_fun/components/divide.ex
+++ lib/unit_fun/components/divide.ex
@@ -1,8 +1,14 @@
 defmodule UnitFun.Components.Divide do
   alias UnitFun.Value
+  import UnitFun.Conversion.ConversionHelper, only: [convert_to: 2]
 
   def divide(%Value{value: left, units: x}, %Value{value: right, units: x}) do
     left / right
+  end
+
+  def divide(%Value{units: left_units} = left, %Value{} = right) do
+    updated_right = right |> convert_to(left_units)
+    divide(left, updated_right)
   end
 
   def divide(%Value{value: left, units: units}, scalar_right) do
--- test/unit_fun_division_test.exs
+++ test/unit_fun_division_test.exs
@@ -3,6 +3,7 @@ defmodule UnitFunDivisionTest do
   alias UnitFun.Value
 
   alias UnitFunDivisionTest.Meters
+  alias UnitFunDivisionTest.Miles
   #alias UnitFunDivisionTest.Newtons
 
 
@@ -13,6 +14,14 @@ defmodule UnitFunDivisionTest do
     value_two = %Value{value: 5, units: Meters.unit}
 
     expected = 2
+    assert value_one |> divide(value_two) == expected
+  end
+
+  test "division of convertable units returns a scalar value" do
+    value_one = %Value{value: 1600, units: Meters.unit}
+    value_two = %Value{value: 1, units: Miles.unit}
+
+    expected = 1
     assert value_one |> divide(value_two) == expected
   end
 
@@ -44,4 +53,13 @@ end
 
 defmodule UnitFunDivisionTest.Miles do
   use UnitFun.Unit
+end
+
+
+defimpl UnitFun.Convertor, for: UnitFunDivisionTest.Meters do
+  alias UnitFunDivisionTest.Miles
+
+  def convert(_, Miles, value) do
+    value * 1.6 * 1000
+  end
 end
