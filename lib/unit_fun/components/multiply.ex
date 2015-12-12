930beb3888bc033daf4f64d0281670466ca1cd3d
steve.brazier <meadsteve@gmail.com>
2015-12-12 16:07:27 +0000

    add UnitFun.Maths.MultiplicationMaths protocol

--- lib/unit_fun/components/multiply.ex
+++ lib/unit_fun/components/multiply.ex
@@ -1,14 +1,17 @@
 defmodule UnitFun.Components.Multiply do
   alias UnitFun.Value
   alias UnitFun.Units.CompositeUnit
+  alias UnitFun.Maths.MultiplicationMaths
 
   def multiply(%Value{value: left, units: x}, %Value{value: right, units: y}) do
     new_units = x |> CompositeUnit.multiply_unit(y)
-    %Value{value: left * right, units: new_units}
+    new_value = MultiplicationMaths.multiply(left, right)
+    %Value{value: new_value, units: new_units}
   end
 
   def multiply(%Value{value: left, units: units}, scalar_right) do
-    %Value{value: left * scalar_right, units: units}
+    new_value = MultiplicationMaths.multiply(left, scalar_right)
+    %Value{value: new_value, units: units}
   end
 
   def multiply(scalar_left, %Value{} = right) do
