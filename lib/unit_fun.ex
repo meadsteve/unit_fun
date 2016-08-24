a2c45fe1369fdc14b764b4f3fd3fb664b9bf5d6d
stephen.brazier <stephen.brazier@crowdcube.com>
2016-08-24 18:22:36 +0100

    alignment

--- lib/unit_fun.ex
+++ lib/unit_fun.ex
@@ -9,10 +9,10 @@ defmodule UnitFun do
   alias UnitFun.Units.UnitSimplifier
   alias UnitFun.UnitTypes
 
-  def equal(left, right),    do: left |> Equality.equal(right) |> simplify
-  def multiply(left, right), do: left |> Multiply.multiply(right) |> simplify
-  def divide(left, right),   do: left |> Divide.divide(right) |> simplify
-  def add(left, right),      do: left |> AddSubtract.add(right) |> simplify
+  def equal(left, right),    do: left |> Equality.equal(right)       |> simplify
+  def multiply(left, right), do: left |> Multiply.multiply(right)    |> simplify
+  def divide(left, right),   do: left |> Divide.divide(right)        |> simplify
+  def add(left, right),      do: left |> AddSubtract.add(right)      |> simplify
   def subtract(left, right), do: left |> AddSubtract.subtract(right) |> simplify
 
   def with_units(value, units),   do: UnitTypes.with_units(value, units)
