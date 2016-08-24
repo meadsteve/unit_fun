f204b41279a67083a660b85f7c8fab634b090c1a
stephen.brazier <stephen.brazier@crowdcube.com>
2016-08-24 16:12:27 +0100

    add credo for code cleanup help

--- mix.exs
+++ mix.exs
@@ -38,6 +38,8 @@ defmodule UnitFun.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [
+      {:credo, "~> 0.4", only: [:dev, :test]}
+    ]
   end
 end
