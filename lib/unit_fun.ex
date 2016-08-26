6be259465b9003cceb4d5eb1d46193103de8b595
stephen.brazier <stephen.brazier@crowdcube.com>
2016-08-26 08:11:52 +0100

    indent docs properly

--- lib/unit_fun.ex
+++ lib/unit_fun.ex
@@ -19,20 +19,20 @@ defmodule UnitFun do
   ## Examples
 
   ### Plain values
-    iex> UnitFun.add(3, 7)
-    10
+      iex> UnitFun.add(3, 7)
+      10
 
   ### Matching units
-    iex> left  = 3 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
-    iex> right = 5 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
-    iex> UnitFun.add(left, right).value
-    8
+      iex> left  = 3 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
+      iex> right = 5 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
+      iex> UnitFun.add(left, right).value
+      8
 
   ### Mismatched units
-    iex> left  = 3 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
-    iex> right = 5 |> UnitFun.with_units(UnitFun.Examples.OtherUnit)
-    iex> UnitFun.add(left, right).value
-    ** (UnitFun.Errors.MissingConversionError) No conversions into Elixir.UnitFun.Examples.SimpleUnit implemented
+      iex> left  = 3 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
+      iex> right = 5 |> UnitFun.with_units(UnitFun.Examples.OtherUnit)
+      iex> UnitFun.add(left, right).value
+      ** (UnitFun.Errors.MissingConversionError) No conversions into Elixir.UnitFun.Examples.SimpleUnit implemented
 
   """
   def add(left, right),      do: left |> AddSubtract.add(right)      |> simplify
@@ -44,20 +44,20 @@ defmodule UnitFun do
   ## Examples
 
   ### Plain values
-    iex> UnitFun.subtract(7, 3)
-    4
+      iex> UnitFun.subtract(7, 3)
+      4
 
   ### Matching units
-    iex> left  = 5 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
-    iex> right = 2 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
-    iex> UnitFun.subtract(left, right).value
-    3
+      iex> left  = 5 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
+      iex> right = 2 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
+      iex> UnitFun.subtract(left, right).value
+      3
 
   ### Mismatched units
-    iex> left  = 5 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
-    iex> right = 3 |> UnitFun.with_units(UnitFun.Examples.OtherUnit)
-    iex> UnitFun.subtract(left, right).value
-    ** (UnitFun.Errors.MissingConversionError) No conversions into Elixir.UnitFun.Examples.SimpleUnit implemented
+      iex> left  = 5 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
+      iex> right = 3 |> UnitFun.with_units(UnitFun.Examples.OtherUnit)
+      iex> UnitFun.subtract(left, right).value
+      ** (UnitFun.Errors.MissingConversionError) No conversions into Elixir.UnitFun.Examples.SimpleUnit implemented
 
   """
   def subtract(left, right), do: left |> AddSubtract.subtract(right) |> simplify
@@ -69,25 +69,25 @@ defmodule UnitFun do
   ## Examples
 
   ### Plain values
-    iex> UnitFun.multiply(2, 3)
-    6
+      iex> UnitFun.multiply(2, 3)
+      6
 
   ### One united value and one plain number
-    iex> left  = 5 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
-    iex> right = 2
-    iex> answer = UnitFun.multiply(left, right)
-    iex> answer |> UnitFun.assert_units(UnitFun.Examples.SimpleUnit)
-    iex> answer.value
-    10
+      iex> left  = 5 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
+      iex> right = 2
+      iex> answer = UnitFun.multiply(left, right)
+      iex> answer |> UnitFun.assert_units(UnitFun.Examples.SimpleUnit)
+      iex> answer.value
+      10
 
   ### Both united numbers
-    iex> left  = 5 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
-    iex> right = 2 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
-    iex> answer = UnitFun.multiply(left, right)
-    iex> square_units = UnitFun.multiply(UnitFun.Examples.SimpleUnit, UnitFun.Examples.SimpleUnit)
-    iex> answer |> UnitFun.assert_units(square_units)
-    iex> answer.value
-    10
+      iex> left  = 5 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
+      iex> right = 2 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
+      iex> answer = UnitFun.multiply(left, right)
+      iex> square_units = UnitFun.multiply(UnitFun.Examples.SimpleUnit, UnitFun.Examples.SimpleUnit)
+      iex> answer |> UnitFun.assert_units(square_units)
+      iex> answer.value
+      10
 
   """
   def multiply(left, right), do: left |> Multiply.multiply(right)    |> simplify
@@ -99,23 +99,23 @@ defmodule UnitFun do
   ## Examples
 
   ### Plain values
-    iex> UnitFun.divide(6, 3)
-    2.0
+      iex> UnitFun.divide(6, 3)
+      2.0
 
   ### Both values have the same units
-    iex> left  = 8 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
-    iex> right = 2 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
-    iex> UnitFun.divide(left, right)
-    4.0
+      iex> left  = 8 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
+      iex> right = 2 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
+      iex> UnitFun.divide(left, right)
+      4.0
 
   ### Both values have units but they are different
-    iex> left  = 8 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
-    iex> right = 2 |> UnitFun.with_units(UnitFun.Examples.OtherUnit)
-    iex> answer = UnitFun.divide(left, right)
-    iex> simple_per_other_units = UnitFun.divide(UnitFun.Examples.SimpleUnit, UnitFun.Examples.OtherUnit)
-    iex> answer |> UnitFun.assert_units(simple_per_other_units)
-    iex> answer.value
-    4.0
+      iex> left  = 8 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
+      iex> right = 2 |> UnitFun.with_units(UnitFun.Examples.OtherUnit)
+      iex> answer = UnitFun.divide(left, right)
+      iex> simple_per_other_units = UnitFun.divide(UnitFun.Examples.SimpleUnit, UnitFun.Examples.OtherUnit)
+      iex> answer |> UnitFun.assert_units(simple_per_other_units)
+      iex> answer.value
+      4.0
 
   """
   def divide(left, right),   do: left |> Divide.divide(right)        |> simplify
@@ -128,20 +128,23 @@ defmodule UnitFun do
   ## Examples
 
   ### Plain values
-    iex> UnitFun.equal(5, 5)
-    true
+
+      iex> UnitFun.equal(5, 5)
+      true
 
   ### Both values have the same value & units
-    iex> left  = 6 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
-    iex> right = 6 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
-    iex> UnitFun.equal(left, right)
-    true
+
+      iex> left  = 6 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
+      iex> right = 6 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
+      iex> UnitFun.equal(left, right)
+      true
 
   ### Both values have the same value but different units
-    iex> left  = 6 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
-    iex> right = 6 |> UnitFun.with_units(UnitFun.Examples.OtherUnit)
-    iex> UnitFun.equal(left, right)
-    ** (UnitFun.Errors.CannotCompareError) Can't compare Elixir.UnitFun.Examples.SimpleUnit to Elixir.UnitFun.Examples.OtherUnit. No conversions into Elixir.UnitFun.Examples.SimpleUnit implemented
+
+      iex> left  = 6 |> UnitFun.with_units(UnitFun.Examples.SimpleUnit)
+      iex> right = 6 |> UnitFun.with_units(UnitFun.Examples.OtherUnit)
+      iex> UnitFun.equal(left, right)
+      ** (UnitFun.Errors.CannotCompareError) Can't compare Elixir.UnitFun.Examples.SimpleUnit to Elixir.UnitFun.Examples.OtherUnit. No conversions into Elixir.UnitFun.Examples.SimpleUnit implemented
 
   """
   def equal(left, right),    do: left |> Equality.equal(right)
@@ -152,15 +155,15 @@ defmodule UnitFun do
   ## Examples
 
   ### Plain values
-    iex> x = UnitFun.with_units(5, UnitFun.Examples.SimpleUnit)
-    iex> x |> UnitFun.assert_units(UnitFun.Examples.SimpleUnit)
-    iex> x.value
-    5
+      iex> x = UnitFun.with_units(5, UnitFun.Examples.SimpleUnit)
+      iex> x |> UnitFun.assert_units(UnitFun.Examples.SimpleUnit)
+      iex> x.value
+      5
 
   ### It will attempt to convert to new units but fail if a conversion isn't defined
-    iex> x = UnitFun.with_units(5, UnitFun.Examples.SimpleUnit)
-    iex> y = UnitFun.with_units(x, UnitFun.Examples.OtherUnit)
-    ** (UnitFun.Errors.MissingConversionError) No conversions into Elixir.UnitFun.Examples.OtherUnit implemented
+      iex> x = UnitFun.with_units(5, UnitFun.Examples.SimpleUnit)
+      iex> y = UnitFun.with_units(x, UnitFun.Examples.OtherUnit)
+      ** (UnitFun.Errors.MissingConversionError) No conversions into Elixir.UnitFun.Examples.OtherUnit implemented
 
   """
   def with_units(value, units),   do: UnitTypes.with_units(value, units)
@@ -171,14 +174,14 @@ defmodule UnitFun do
   ## Examples
 
   ### valid assertion - no error thrown
-    iex> x = UnitFun.with_units(5, UnitFun.Examples.SimpleUnit)
-    iex> y = x |> UnitFun.assert_units(UnitFun.Examples.SimpleUnit)
-    iex> y.value
-    5
+      iex> x = UnitFun.with_units(5, UnitFun.Examples.SimpleUnit)
+      iex> y = x |> UnitFun.assert_units(UnitFun.Examples.SimpleUnit)
+      iex> y.value
+      5
 
   ### Returns false when values don't have units
-    iex> 6 |> UnitFun.assert_units(UnitFun.Examples.SimpleUnit)
-    ** (UnitFun.Errors.UnitAssertionError) Expected value with units Elixir.UnitFun.Examples.SimpleUnit; Got: 6
+      iex> 6 |> UnitFun.assert_units(UnitFun.Examples.SimpleUnit)
+      ** (UnitFun.Errors.UnitAssertionError) Expected value with units Elixir.UnitFun.Examples.SimpleUnit; Got: 6
 
   """
   def assert_units(value, units), do: Assert.assert_units(value, units)
