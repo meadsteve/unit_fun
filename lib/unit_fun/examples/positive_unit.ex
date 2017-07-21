fb7130a3a481205cb18fb4234125f7fb0d523b6c
steve.brazier <steve.brazier@just-eat.com>
2017-07-21 08:51:00 +0100

    update README

--- lib/unit_fun/examples/positive_unit.ex
+++ lib/unit_fun/examples/positive_unit.ex
@@ -2,8 +2,10 @@ defmodule UnitFun.Examples.PositiveUnit do
   @moduledoc false
   use UnitFun.Unit
 
+  defp greater_than_zero(x), do: x >= 0
+
   facts [
-    fn x -> x >= 0 end
+    &greater_than_zero/1
   ]
 
 end
